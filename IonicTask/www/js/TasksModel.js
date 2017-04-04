b2ff9430fe4cfec1cfb19d6d787617769fb6da21
Wagner Almeida <wdmeida@gmail.com>
2017-04-03 22:36:48 -0300

    Implementação do crud e persistênsia das tarefas.

--- IonicTask/www/js/TasksModel.js
+++ IonicTask/www/js/TasksModel.js
@@ -1,12 +1,21 @@
 function getTasks() {
-  this.items = [ {
-      nome: 'Item 01',
-      finalizada: false
-  }, {
-    nome: 'Item 02',
-    finalizada: false
-  }, {
-    nome: 'Item 03',
-    finalizada: false
-  }];
+  this.items = [];
+
+  var lista = localStorage.getItem("taskList");
+
+  if (lista !== null) this.items = angular.fromJson(lista);
+
+  this.save = function() {
+    var lista = angular.toJson(this.items);
+    localStorage.setItem("taskList", lista);
+  };
+
+  this.add = function (item){
+    this.items.push(item);
+  };
+
+  this.remove = function (item) {
+    var pos = this.items.indexOf(item);
+    this.items.splice(pos, 1);
+  };
 }
--- IonicTask/www/js/app.js
+++ IonicTask/www/js/app.js
@@ -23,18 +23,64 @@ app.run(function($ionicPlatform) {
   });
 });
 
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $ionicPopup, $ionicListDelegate) {
   var tasks = new getTasks();
 
   $scope.lista = tasks.items;
-
   $scope.showMarked = false;
+  $scope.removeStatus = false;
+
+  function getItem(item, novo) {
+    $scope.data = {};
+    $scope.data.newTask = item.nome;
+
+    $ionicPopup.show({
+      title: 'Nova Tarefa',
+      scope: $scope,
+      template: '<input type="text" placeholder="Tarefa" autofocus="true" ng-model="data.newTask">',
+      buttons: [
+        {
+          text: 'Ok',
+          onTap: function(e){
+            item.nome = $scope.data.newTask;
+
+            if (novo) tasks.add(item);
+            tasks.save();
+          }
+        }, {
+          text: 'Cancel'
+        }
+      ]
+    });
+    $ionicListDelegate.closeOptionButtons();
+  };
 
   $scope.onMarkTask = function (item) {
     item.finalizada = !item.finalizada;
+    tasks.save();
   };
 
   $scope.onHideItem = function (item) {
     return item.finalizada && !$scope.showMarked;
   };
+
+  $scope.onItemAdd = function() {
+    var item = {nome: '', finalizada: false};
+    getItem(item, true);
+  };
+
+  $scope.onItemEdit = function(item) {
+    getItem(item, false);
+  };
+
+  $scope.onItemRemove = function(item) {
+    tasks.remove(item);
+    tasks.save();
+  };
+
+  $scope.onClickRemove = function(item) {
+    console.log("Olha eu aqui");
+    $scope.removeStatus = !$scope.removeStatus;
+  };
+
 });
